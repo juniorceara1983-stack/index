a70d831fea4aecca01a54754f9d3e380d647e263
juniorceara1983-stack <juniorceara1983@gmail.com>
2026-03-30 10:58:07 -0300

    Implement service worker caching for assets

--- sw.js
+++ sw.js
@@ -1,11 +1,20 @@
+const CACHE_NAME = 'eventos-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json'
+];
+
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    fetch(e.request).catch(() => {
-      return new Response("Você está offline.");
-    })
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
