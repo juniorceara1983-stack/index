e3af67c21a7594d21c56abe8b324ab0334e2094e
juniorceara1983-stack <juniorceara1983@gmail.com>
2026-04-02 10:45:48 -0300

    Create sw-eventos.js

---
+++ sw-eventos.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'cache-eventos-v1';
+const assets = [
+  './index.html',
+  './manifest-eventos.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
